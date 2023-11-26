c5fd3938efb2cc49b63d8399e44313dd5ec63759
agustinE13 <20030136@itcelaya.edu.mx>
2023-11-25 23:54:38 -0600

    cambio en la generacion de cookies

--- index.js
+++ index.js
@@ -9,16 +9,17 @@ const apiprefix = process.env.API_PREFIX
 const port = process.env.PORT
 const app = express()
 const corsOptions = {
-    origin: ['http://localhost:8000', 'http://127.0.0.1:4200', 'http://172.19.191.78:8000'],  // Puedes ajustar el origen permitido
+    origin: ['http://localhost:4200', 'http://127.0.0.1:4200'],  // Puedes ajustar el origen permitido
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   };
 
   //middleware
 app.use(express.json())
+app.use(cookieParser())
 app.use(cors(corsOptions));
 //administraar cookies
-app.use(cookieParser())
+
 app.use(passport.initialize())
 //routes
 app.use(apiprefix,producto)
