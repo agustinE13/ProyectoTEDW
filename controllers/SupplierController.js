47753f51473e45a8490dcaae1fd4cec0245466f2
agustinE13 <20030136@itcelaya.edu.mx>
2023-11-27 17:41:05 -0600

    controlador y rutas para crud supplier

--- controllers/SupplierController.js
+++ controllers/SupplierController.js
@@ -1,27 +1,90 @@
-const mongoose = require('mongoose')
-const Supplier = require('../models/Supplier')
+const mongoose = require('mongoose');
+const Supplier = require('../models/Supplier');
 
-const newsupplier= async (req,res)=>{
+const newsupplier = async (req, res) => {
     try {
-        const supplierexist = await Supplier.findOne({supplier: req.body.supplier})
-        let supplier = new Supplier({
-            supplier: req.params.supplier
-        })
-        if(supplierexist){
-            res.status(400).send("There is already a supplier with that name")
+        const supplierExist = await Supplier.findOne({ supplier: req.body.supplier });
+
+        if (supplierExist) {
+            return res.status(400).json({ success: false, message: "There is already a supplier with that name" });
         }
-       supplier = await supplier.save()
-       res.status(200).json({sucess: true, message: "added supplier"})
+        const newSupplier = new Supplier({
+            supplier: req.body.supplier
+        });
+        const savedSupplier = await newSupplier.save();
+        res.status(200).json({ success: true, message: "Added supplier"});
     } catch (error) {
-         res.status(500).json({ success: false, message: "Internal server error" });
+        console.error(error);
+        res.status(500).json({ success: false, message: "Internal server error" });
     }
-}
-const getsuppliers = async (req,res)=>{
+};
+
+const getsuppliers = async (req, res) => {
     try {
-        
+        const suppliers = await Supplier.find()
+        res.send(suppliers)
     } catch (error) {
-        
+        console.error(error);
+        res.status(500).json({ success: false, message: "Internal server error" });
     }
+};
+
+const updatesupplier = async (req,res)=>{
+    Supplier.findOneAndUpdate({ _id: req.params.id }, { $set: req.body }, { new: true })
+      .then(supplier => {
+        if (supplier) {
+          return res.status(200).json({ success: true, message: 'Updated supplier' });
+        } else {
+          return res.status(404).json({ success: false, message: 'supplier did not update' });
+        }
+      })
+      .catch(err => {
+        return res.status(500).json({ success: false, error: err });
+      });
 }
 
-module.exports = {newsupplier,getsuppliers}
+const deletesupplier = async ( req,res)=>{
+    Supplier.findByIdAndRemove(req.params.id).then(supplier =>{
+        if(supplier){
+            return res.status(200).json({success: true, message: 'removed supplier'})
+        }else{
+            return res.status(404).json({success: false, message: 'supplier does not exist'})
+        }
+    }).catch(err=>{
+        return res.status(500).json({success: false, error: err}) 
+    })
+}
+
+const supplierId = async (req,res)=>{
+    try {
+        const result = await Supplier.findOne({ _id: req.params.id });
+
+        if (result) {
+            res.send(result);
+        } else {
+            res.status(404).json({ success: false, message: 'The product was not found' });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, error: 'Internal server error' });
+    }
+}
+const search = async (req, res) => {
+    const { item } = req.params;
+    
+    try {
+      const supplier = await Supplier.find({
+        $or: [
+          { supplier: new RegExp('^' + item, 'i') },
+        ],
+      });
+  
+      res.json(supplier);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  };
+
+
+module.exports = {newsupplier,getsuppliers,updatesupplier,deletesupplier,supplierId,search}
--- routes/RouteSupplier.js
+++ routes/RouteSupplier.js
@@ -4,8 +4,16 @@ const supplierController = require('../controllers/SupplierController')
 const checkOrigin = require('../middleware/origin')
 const checkAuth = require('../middleware/auth')
 const checkRoleAuth = require('../middleware/roleauth')
-const { route } = require('./RouteUsuario')
 
+//-----Gets
+router.get('/suppliers',supplierController.getsuppliers)
+router.get('/supplier/:id', supplierController.supplierId)
+router.get('/searchsupplier/:item',supplierController.search)
+//--Posts
 router.post('/Newsupplier',supplierController.newsupplier)
+//---put
+router.put('/updatesupplier/:id',supplierController.updatesupplier)
+//---delete
+router.delete('/deletesupplier/:id',supplierController.deletesupplier)
 
 module.exports = router
